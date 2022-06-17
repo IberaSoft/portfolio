dd4877a60c5a9f8b834e6b8026cdabd124793589
Juan Llorens <526473+gb96@users.noreply.github.com>
2022-06-17 18:59:28 +0200

    Added i18next for translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+import { Suspense } from 'react';
+import Footer from './Footer'
+
+const language:any = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(language).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {language[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -16,11 +34,19 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('description.part2')}
         </a>
       </header>
+      <Footer t={t} />
     </div>
   );
 }
 
-export default App;
+// here app catches the suspense from page in case translations are not yet loaded
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="...is loading">
+      <App />
+    </Suspense>
+  );
+}
