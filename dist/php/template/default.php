be6c9d1c56421af2c450f1fa0068565aa3031dca
Juan Llorens <526473+gb96@users.noreply.github.com>
2022-12-16 12:09:44 +0100

    last working version in production

---
+++ dist/php/template/default.php
@@ -0,0 +1,47 @@
+<? 
+	ob_start();
+?>
+		<html>
+
+			<head>
+		
+			</head>
+		
+			<body>
+			
+				<div><? echo $message; ?></div>
+			
+			</body>
+			
+		</html>
+<?
+	$contents=ob_get_contents();
+   ob_end_clean();
+   return($contents);
+?>
+
+
+	<? 
+	ob_start();
+?>
+		<html>
+
+			<head>
+		
+			</head>
+		
+			<body>
+			
+				<div><? echo $message; ?></div>
+			
+			</body>
+			
+		</html>
+<?
+	$contents=ob_get_contents();
+   ob_end_clean();
+   return($contents);
+?>
+
+
+	
